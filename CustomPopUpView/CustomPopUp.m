f2b6a653518fd3c01089375cf8ba9734f3bf7acd
Bhavin Gupta <bhavin@easypay.in>
2017-06-19 15:34:17 +0530

    pop up view
    major changes in animation while adding view.

--- CustomPopUpView/CustomPopUp.m
+++ CustomPopUpView/CustomPopUp.m
@@ -14,41 +14,20 @@
 -(id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if(self){
-        self = [[[NSBundle mainBundle] loadNibNamed:@"CustomPopUp"
+        self = [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class])
                                               owner:self
                                             options:nil] objectAtIndex:0];
         self.frame = frame;
-        [self showPopUp];
     }
     return self;
 }
 
 #pragma mark - Button Action Method
 - (IBAction)closeButtonClicked:(UIButton*)sender {
-    [self closePopUp];
-}
-
-#pragma mark - Custom Open & Close Method
-- (void)showPopUp{
-    self.transform = CGAffineTransformMakeScale(1.4, 1.4);
-    self.alpha = 0.0f;
-    [UIView animateWithDuration:0.25f animations:^{
-        self.alpha = 1.0f;
-        self.transform = CGAffineTransformMakeScale(1.0, 1.0);
-    } completion:^(BOOL finished) {
-    }];
-}
-
-- (void)closePopUp{
-    [UIView animateWithDuration:0.25f animations:^{
-        self.alpha = 0.0f;
-        self.transform = CGAffineTransformMakeScale(1.4, 1.4);
-    } completion:^(BOOL finished) {
-        [self removeFromSuperview];
-        if([self.customPopUpDelegate respondsToSelector:@selector(dismissCustomPopUpView:)]){
-            [self.customPopUpDelegate dismissCustomPopUpView:self];
-        }
-    }];
+    [self removeFromSuperview];
+    if([self.customPopUpDelegate respondsToSelector:@selector(dismissCustomPopUpView:)]){
+        [self.customPopUpDelegate dismissCustomPopUpView:self];
+    }
 }
 
 @end
